d496652d3e414eade06e493991aa979a8d0ef817
Matt <aukrais@gmail.com>
2021-11-28 11:58:07 -0700

    Initial commit

---
+++ src/main/java/com/superyuuki/yuukonstants/DeveloperFailure.java
@@ -0,0 +1,23 @@
+package com.superyuuki.yuukonstants;
+
+public class DeveloperFailure extends Failure {
+
+    public DeveloperFailure() {
+    }
+
+    public DeveloperFailure(String message) {
+        super(message);
+    }
+
+    public DeveloperFailure(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeveloperFailure(Throwable cause) {
+        super(cause);
+    }
+
+    public DeveloperFailure(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
